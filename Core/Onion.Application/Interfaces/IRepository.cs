da8084f1273edcdd3580b52ad434beada5084744
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-31 16:41:06 +0300

    Entities id types changed to GUID, IRepository created

---
+++ Core/Onion.Application/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Onion.Application.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+    }
+}
--- Core/Onion.Domain/Entities/Product.cs
+++ Core/Onion.Domain/Entities/Product.cs
@@ -2,11 +2,11 @@
 {
     public class Product
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public required string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public int CategoryId { get; set; }
+        public Guid CategoryId { get; set; }
         public required Category Category { get; set; }
     }
 }
