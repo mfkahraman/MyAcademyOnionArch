7f6dcd7cc10eeccde765cc1b09980296d27339c9
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-31 16:54:30 +0300

    DbContext and connection confg created

---
+++ Infrastructure/Onion.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Onion.Domain.Entities;
+
+namespace Onion.Persistance.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Infrastructure/Onion.Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Onion.Application.Interfaces;
+
+namespace Onion.Persistance.Repositories
+{
+    internal class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public Task CreateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
