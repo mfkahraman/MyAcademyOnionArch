04f1061d356065fd781c26632f75b3d9950da1b3
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-02 19:35:59 +0300

    Create product mediatr classes created

---
+++ Core/Onion.Application/Features/Mediator/Commands/CreateProductCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Onion.Application.Features.Mediator.Commands
+{
+    public record CreateProductCommand : IRequest<bool>
+    {
+        public required string Name { get; init; }
+        public decimal Price { get; init; }
+        public int Stock { get; init; }
+        public Guid CategoryId { get; init; }
+    }
+}
---
+++ Core/Onion.Application/Features/Mediator/Handlers/CreateProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Onion.Application.Features.Mediator.Commands;
+using Onion.Application.Interfaces;
+using Onion.Domain.Entities;
+
+namespace Onion.Application.Features.Mediator.Handlers
+{
+    public class CreateProductCommandHandler(IRepository<Product> repository, IMapper mapper)
+        : IRequestHandler<CreateProductCommand, bool>
+    {
+        public async Task<bool> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = mapper.Map<Product>(request);
+            try
+            {
+                await repository.CreateAsync(product);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
